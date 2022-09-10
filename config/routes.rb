6dc8d514e4a8a967280063b4ae8732f41aeccf50
Eduardo Santos <eduardo.smiles@gmail.com>
2022-09-10 20:48:27 -0300

    first commit

---
+++ config/routes.rb
@@ -0,0 +1,17 @@
+Rails.application.routes.draw do
+  namespace :site do
+    get 'welcome/index'
+  end
+  namespace :users_backoffice do
+    get 'welcome/index'
+  end
+  namespace :admins_backoffice do
+    get 'welcome/index'
+  end
+  devise_for :admins
+  devise_for :users
+  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+
+  # Defines the root path route ("/")
+  root to: 'site/welcome#index'
+end
